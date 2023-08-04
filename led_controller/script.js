c72df7d6a32d48a63ff6b63955f62455b8beefe6
jorrel1230 <140212785+jorrel1230@users.noreply.github.com>
2023-08-04 12:18:48 -0400

    Update script.js

--- led_controller/script.js
+++ led_controller/script.js
@@ -1,18 +1,14 @@
-endpoint = 'https://cloud.appwrite.io/v1/databases/led/collections/led/documents/doc';
+endpoint = 'https://cloud.appwrite.io/v1/databases/led_base/collections/led_coll/documents/led_doc';
 
 params = {
     'Content-Type': 'application/json',
-    'X-Appwrite-Project': '64cd1b0e1bd434ed947c'
+    'X-Appwrite-Project': 'jorrel_led_proj',
+    'X-Appwrite-Key': '9f4c8ddd2dc03a7c1b3e91bb5f2ac7ad687af8eb531a4a5a461be7df5268da2d32091a040f4e49a148e3ff6b175b2e379ca28715445c960948eb8971b119aa69b41301318c8a1442d25bdeffc88268978da4014098838a1b6a0518c79ea01fbe581d6fa0efde556ad2b88ff59eeefa259ec2dc77aa336e3835cde528412305a0'
 };
-
-update = {
-    
-};
-
-data_send = JSON.stringify(update);
 
 setInterval(function() {
-    fetch(endpoint, {method: 'PATCH', headers:params, body:data_send})
+    console.log("fetching...");
+    fetch(endpoint, {headers:params})
     .then((data) => data.json())
     .then((info) => {
         console.log(info);
