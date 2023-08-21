068fc072db77f75fbbc9a9ee6bdaa33975671f10
jorrel1230 <jr8867@princeton.edu>
2023-08-21 14:33:09 -0400

    big changes ?!?!?!?!?!?!??!?!!!!!!!!!!!!!!!!!!
    poopity scoop scoopity poop

--- script.js
+++ script.js
@@ -8,6 +8,39 @@ function led_redirect() {
 }
 
 
+/* NAVIGATION BUTTON FUNCTIONS */
+function aboutme() {
+    window.location.href = "#";
+}
+
+function projects() {
+    window.location.href = "#projects";
+}
+
+
+
+/* ICON BUTTON FUNCTIONS */
+function insta() {
+    window.location.href = "https://www.instagram.com/_jorrel/";
+}
+
+function discord() {
+    window.location.href = "https://discord.com/users/304680442349748224";
+}
+
+function linkedin() {
+    window.location.href = "https://jorrelrajan.com/temp";
+}
+
+function email() {
+    window.location.href = "mailto:jorrel@princeton.edu";
+}
+
+function github() {
+    window.location.href = "https://github.com/jorrel1230";
+}
+
+
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
