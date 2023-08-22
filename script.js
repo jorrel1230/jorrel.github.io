2a58043325a0ac47af6cea45912db705be26092a
jorrel1230 <jr8867@princeton.edu>
2023-08-22 00:20:39 -0400

    W

--- script.js
+++ script.js
@@ -18,30 +18,6 @@ function projects() {
 }
 
 
-
-/* ICON BUTTON FUNCTIONS */
-function insta() {
-    window.location.href = "https://www.instagram.com/_jorrel/";
-}
-
-function discord() {
-    window.location.href = "https://discord.com/users/304680442349748224";
-}
-
-function linkedin() {
-    window.location.href = "https://jorrelrajan.com/temp";
-}
-
-function email() {
-    window.location.href = "mailto:jorrel@princeton.edu";
-}
-
-function github() {
-    window.location.href = "https://github.com/jorrel1230";
-}
-
-
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
